34ebc532ac475255526362daa307ccb0de3759bb
kadirnar <kadir.nar@hotmail.com>
2022-01-25 01:34:25 +0300

    class bilgileri coco_classes.py dosyasına aktarılmıştır.

--- detect.py
+++ detect.py
@@ -2,26 +2,14 @@ import torchvision
 import cv2
 from PIL import Image
 from torchvision import transforms as T
+import coco_classes
 
 model_pretrained = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
 # Inference
 model_pretrained.eval()
 
-COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
+print(coco_classes.classes)
 
 
 def get_prediction(img_path, threshold):
@@ -29,7 +17,7 @@ def get_prediction(img_path, threshold):
     transform = T.Compose([T.ToTensor()])  # Defing PyTorch Transform
     img = transform(img)  # Apply the transform to the image
     pred = model_pretrained([img])  # Pass the image to the model
-    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())]  # Get the Prediction Score
+    pred_class = [coco_classes.classes[i] for i in list(pred[0]['labels'].numpy())]  # Get the Prediction Score
     pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]  # Bounding boxes
     pred_score = list(pred[0]['scores'].detach().numpy())
     pred_t = [pred_score.index(x) for x in pred_score if x > threshold][
@@ -53,4 +41,4 @@ def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=2, text_t
     cv2.destroyAllWindows()
 
 
-object_detection_api('images/00acf53b127218c2.jpg', threshold=0.8)
+object_detection_api('images/1.jpg', threshold=0.8)
