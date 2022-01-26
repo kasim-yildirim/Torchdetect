9d779ee596166a1a87da3739cceb075e0787f0af
kadirnar <kadir.nar@hotmail.com>
2022-01-27 01:14:24 +0300

    detect kodunda score ve img_size değişkenleri eklendi.

--- coco_classes.py
+++ coco_classes.py
@@ -1,3 +1,7 @@
+import cv2
+import numpy as np
+import torch
+
 classes = (
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
@@ -12,3 +16,41 @@ classes = (
     'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
     'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
 )
+
+
+def read_image(image, img_size=416):
+    if type(image) == str:
+        img = cv2.imread(image)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (img_size, img_size))
+
+    elif type(image) == bytes:
+        nparr = np.frombuffer(image, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (img_size, img_size))
+
+    elif type(image) == np.ndarray:
+        if len(image.shape) == 2:  # grayscale
+            img = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+            img = cv2.resize(img, (img_size, img_size))
+
+        elif len(image.shape) == 3 and image.shape[2] == 3:
+            img = image
+            img = cv2.resize(img, (img_size, img_size))
+
+        elif len(image.shape) == 3 and image.shape[2] == 4:  # RGBAscale
+            img = image[:, :, :3]
+            img = cv2.resize(img, (img_size, img_size))
+
+    img = numpy_to_torch(img)
+
+    return img
+
+
+def numpy_to_torch(img):
+    img = img.transpose((2, 0, 1))
+    img = torch.from_numpy(img).float()
+    if img.max() > 1:
+        img /= 255
+    return img
--- detect.py
+++ detect.py
@@ -1,21 +1,16 @@
-import glob
-import os
-import torch
-import coco_classes
+from coco_classes import classes, read_image
 import cv2
 import torchvision
 
 
-def detect(image_path, model, threshold=0.5):
-    prediction = model([image_path])
-    prediction_class = [coco_classes.classes[i] for i in list(prediction[0]['labels'].numpy())]
+def detect(image, model, threshold=0.5):
+    prediction = model([image])
+    prediction_class = [classes[i] for i in list(prediction[0]['labels'].numpy())]
     prediction_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(prediction[0]['boxes'].detach().numpy())]
     prediction_score = list(prediction[0]['scores'].detach().numpy())
     prediction_thresh = [prediction_score.index(x) for x in prediction_score if x > threshold][-1]
     prediction_boxes = prediction_boxes[:prediction_thresh + 1]
     prediction_class = prediction_class[:prediction_thresh + 1]
-    img = cv2.imread(image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     for i, box in enumerate(prediction_boxes):
         score = prediction_score[i]
@@ -24,28 +19,17 @@ def detect(image_path, model, threshold=0.5):
         class_names = prediction_class[:prediction_thresh + 1]
         labels = '%{} {}'.format(percent_score, class_names[i])
         x, y, w, h = box[0][0], box[0][1], box[1][0] - box[0][0], box[1][1] - box[0][1]
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, labels, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
-def transforms_img(image_path, img_size=416):
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
-    image_path = transform(image_path)
-    image_path = image_path.unsqueeze(0)
-    return image_path
+        image = image.numpy()
+        image = image.transpose((1, 2, 0))
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(image, labels, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    image_path = './images/1.jpg'
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     model.eval()
-    image_path = cv2.imread(image_path)
-    image_path = cv2.cvtColor(image_path, cv2.COLOR_BGR2RGB)
-    image_path = transforms_img(image_path)
-    detect(image_path, model)
+    image = read_image("images/2.jpg", 512)
+    detect(image, model, threshold=0.5)
