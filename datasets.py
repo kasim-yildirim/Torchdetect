66e13f6086f1d1123e7e5d540afecceba3f6345f
kadirnar <kadir.nar@hotmail.com>
2022-01-28 02:41:31 +0300

    datasets.py dosyasında resize ve veri tipi dönüşüm kodları yazıldı.

---
+++ datasets.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import torch
+
+classes = (
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+)
+
+
+def read_image(img):
+    if type(img) == str:
+        img = cv2.imread(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    elif type(img) == bytes:
+        nparr = np.frombuffer(img, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    elif type(img) == np.ndarray:
+        if len(img.shape) == 2:  # grayscale
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+        elif len(img.shape) == 3 and img.shape[2] == 3:
+            img = img
+
+        elif len(img.shape) == 3 and img.shape[2] == 4:  # RGBA
+            img = img[:, :, :3]
+
+    return img
+
+
+def numpy_to_torch(img):
+    img = img.transpose((2, 0, 1))
+    img = torch.from_numpy(img).float()
+    if img.max() > 1:
+        img /= 255
+    return img
+
+
+def torch_to_numpy(img):
+    img = img.numpy()
+    if img.max() > 1:
+        img /= 255
+    img = img.transpose((1, 2, 0))
+    return img
+
+
+def save_image(img, path):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(path, img)
+
+
+def resize_image(image, long_size, interpolation=cv2.INTER_LINEAR):
+    height, width, channel = image.shape
+
+    # set target image size
+    target_size = long_size
+
+    ratio = target_size / max(height, width)
+
+    target_h, target_w = int(height * ratio), int(width * ratio)
+    image = cv2.resize(image, (target_w, target_h), interpolation=interpolation)
+
+    return image
--- detect.py
+++ detect.py
@@ -1,4 +1,4 @@
-from coco_classes import classes, read_image
+from datasets import *
 import cv2
 import torchvision
 import torch
@@ -6,8 +6,7 @@ import torch
 
 def detect(image, model, threshold=0.5):
     prediction = model([image])
-    image = image.numpy()
-    image = image.transpose((1, 2, 0))
+    image = torch_to_numpy(image)  # görselleştirme için resimler numpy formatında olmalıdır.
     prediction_class = [classes[i] for i in list(prediction[0]['labels'].numpy())]
     prediction_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(prediction[0]['boxes'].detach().numpy())]
     prediction_score = list(prediction[0]['scores'].detach().numpy())
@@ -22,17 +21,17 @@ def detect(image, model, threshold=0.5):
         class_names = prediction_class[:prediction_thresh + 1]
         labels = '%{} {}'.format(percent_score, class_names[i])
         x, y, w, h = box[0][0], box[0][1], box[1][0] - box[0][0], box[1][1] - box[0][1]
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(image, labels, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        cv2.putText(image, labels, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
     cv2.imshow('image', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    # torch model load
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True) # .pth dosyasının orijinal modeli
-    model.load_state_dict(torch.load('model.pth')) # kendi .pth dosyasını buraya yükleyin.
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model.load_state_dict(torch.load('model.pth'))
     model.eval()
-    image = read_image("images/1.jpg", 512)
+    image = read_image("images/1.jpg")
+    image = numpy_to_torch(image) # modele giren resimler tensor formatında olmalıdır.
     detect(image, model, threshold=0.5)
