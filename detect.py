0b9175ed5684b24b2b8468153c7ace1b66f795d6
kadirnar <kadir.nar@hotmail.com>
2022-02-05 22:13:04 +0300

    Değişiklikler:
    - Segmentasyon kodları fonksiyon olarak yazıldı
    
    - Url üzerinden otomatik indirme fonksiyonları yazıldı
    
    - detect kodları daha basit hale getirildi.

--- detect.py
+++ detect.py
@@ -1,140 +1,65 @@
-import random
-from PIL import Image
-from matplotlib import pyplot as plt
+import torch
 from utils.datasets import numpy_to_torch, torch_to_numpy, coco_classes
-from utils.object_counter import counter
-from utils.vis import object_visulization
-from utils.img_utils import read_image, save_image, resize_image
+from utils.vis import object_visulization, segmentation_visulization
+from utils.img_utils import read_image
 import cv2
 import torchvision
-import torch
-import numpy as np
-from torchvision import transforms as T
 
 
-def object_detect(image, model, threshold=0.5):
+def object_detections(image, model, threshold=0.5):
+    image = read_image(image)
+    image = numpy_to_torch(image)
     prediction = model([image])
-    print(prediction)
-    image = torch_to_numpy(image)  # görselleştirme için resimler numpy formatında olmalıdır.
+    image = torch_to_numpy(image)
     prediction_class = [coco_classes[i] for i in list(prediction[0]['labels'].numpy())]
     prediction_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(prediction[0]['boxes'].detach().numpy())]
     prediction_score = list(prediction[0]['scores'].detach().numpy())
     prediction_thresh = [prediction_score.index(x) for x in prediction_score if x > threshold][-1]
     prediction_boxes = prediction_boxes[:prediction_thresh + 1]
     prediction_class = prediction_class[:prediction_thresh + 1]
-    class_names = prediction_class[:prediction_thresh + 1]
-    return image, prediction_boxes, class_names, prediction_score
+    return image, prediction_boxes, prediction_class, prediction_score
 
 
-def instance_segmentation_detect(image, model, threshold=0.5):
-    prediction = model([image])
-    print(prediction)
-    image = torch_to_numpy(image)  # görselleştirme için resimler numpy formatında olmalıdır.
-    prediction_class = [coco_classes[i] for i in list(prediction[0]['labels'].numpy())]
-    prediction_segmentation = list(prediction[0]['masks'].detach().numpy())
-    prediction_score = list(prediction[0]['scores'].detach().numpy())
-    prediction_thresh = [prediction_score.index(x) for x in prediction_score if x > threshold][-1]
-    prediction_segmentation = prediction_segmentation[:prediction_thresh + 1]
-    prediction_class = prediction_class[:prediction_thresh + 1]
-    class_names = prediction_class[:prediction_thresh + 1]
-    return image, prediction_segmentation, class_names, prediction_score
-
-
-def get_coloured_mask(mask):
-    """
-  random_colour_masks
-    parameters:
-      - image - predicted masks
-    method:
-      - the masks of each predicted object is given random colour for visualization
-  """
-    colours = [[0, 255, 0], [0, 0, 255], [255, 0, 0], [0, 255, 255], [255, 255, 0], [255, 0, 255], [80, 70, 180],
-               [250, 80, 190], [245, 145, 50], [70, 150, 250], [50, 190, 190]]
-    r = np.zeros_like(mask).astype(np.uint8)
-    g = np.zeros_like(mask).astype(np.uint8)
-    b = np.zeros_like(mask).astype(np.uint8)
-    r[mask == 1], g[mask == 1], b[mask == 1] = colours[random.randrange(0, 10)]
-    coloured_mask = np.stack([r, g, b], axis=2)
-    return coloured_mask
-
-
-def get_prediction(img_path, confidence):
-    """
-    get_prediction
-      parameters:
-        - img_path - path of the input image
-        - confidence - threshold to keep the prediction or not
-      method:
-        - Image is obtained from the image path
-        - the image is converted to image tensor using PyTorch's Transforms
-        - image is passed through the model to get the predictions
-        - masks, classes and bounding boxes are obtained from the model and soft masks are made binary(0 or 1) on masks
-          ie: eg. segment of cat is made 1 and rest of the image is made 0
-
-    """
-    img = Image.open(img_path)
-    transform = T.Compose([T.ToTensor()])
-    img = transform(img)
+def instance_segmentations(img_path, confidence):
+    img = read_image(img_path)
+    img = numpy_to_torch(img)
     pred = model([img])
     pred_score = list(pred[0]['scores'].detach().numpy())
     pred_t = [pred_score.index(x) for x in pred_score if x > confidence][-1]
     masks = (pred[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
-    # print(pred[0]['labels'].numpy().max())
-    pred_class = [coco_classes[i] for i in list(pred[0]['labels'].numpy())]
-    pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
+    prediction_class = [coco_classes[i] for i in list(pred[0]['labels'].numpy())]
+    prediction_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
     masks = masks[:pred_t + 1]
-    pred_boxes = pred_boxes[:pred_t + 1]
-    pred_class = pred_class[:pred_t + 1]
-    return masks, pred_boxes, pred_class
+    prediction_boxes = prediction_boxes[:pred_t + 1]
+    prediction_class = prediction_class[:pred_t + 1]
+    prediction_score = pred_score[:pred_t + 1]
+    return masks, prediction_boxes, prediction_class, prediction_score
 
 
-def segment_instance(img_path, confidence=0.5, rect_th=2, text_size=2, text_th=2):
-    """
-  segment_instance
-    parameters:
-      - img_path - path to input image
-      - confidence- confidence to keep the prediction or not
-      - rect_th - rect thickness
-      - text_size
-      - text_th - text thickness
-    method:
-      - prediction is obtained by get_prediction
-      - each mask is given random color
-      - each mask is added to the image in the ration 1:0.8 with opencv
-      - final output is displayed
-  """
-    masks, boxes, pred_cls = get_prediction(img_path, confidence)
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    for i in range(len(masks)):
-        rgb_mask = get_coloured_mask(masks[i])
-        img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
-        cv2.rectangle(img, boxes[i][0], boxes[i][1], color=(0, 255, 0), thickness=rect_th)
-        cv2.putText(img, pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), thickness=text_th)
-    plt.figure(figsize=(20, 30))
-    plt.imshow(img)
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
+def detections_predictions(image, threshold=0.7):
 
-
-def predictions(image, model, threshold=0.5):
-    image, prediction_boxes, class_names, prediction_score = object_detect(image, model, threshold)
+    image, prediction_boxes, class_names, prediction_score = object_detections(image, model, threshold)
     for i, box in enumerate(prediction_boxes):
-        score = prediction_score[i]
-        counter(image, class_names)
-        object_visulization(image, box, class_names, score, i)
+        image = object_visulization(image, box, class_names, prediction_score, i)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # cv2.imshow('image', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+
+def segmentations_predictions(img_path, confidence=0.5, rect_th=2, text_size=2, text_th=2):
+    masks, boxes, prediction_class, prediction_score = instance_segmentations(img_path, confidence)
+    img = read_image(img_path)
+    for i in range(len(masks)):
+        img = segmentation_visulization(img, masks, boxes, prediction_class,
+                                        prediction_score, text_size, text_th, rect_th, i)
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-    model.load_state_dict(torch.load('mask.pth'))
+    model.load_state_dict(torch.load('models/maskrcnn_resnet50_fpn_coco.pth'))
     model.eval()
-    image = read_image("images/3.jpg")
-    image = numpy_to_torch(image)  # modele giren resimler tensor formatında olmalıdır.
-    # predictions(image, model, threshold=0.7)
-    segment_instance("images/3.jpg")
+    #segmentations_predictions("images/3.jpg")  # instance segmentations
+    #detections_predictions("images/3.jpg")  # object detections
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,4 +1,5 @@
 import os
+import gdown
 
 
 def create_dir(_dir):
@@ -7,3 +8,25 @@ def create_dir(_dir):
     """
     if not os.path.exists(_dir):
         os.makedirs(_dir)
+
+
+def download(url: str, save_path: str):
+    """
+    Downloads file from gdrive, shows progress.
+    Example inputs:
+        url: 'ftp://smartengines.com/midv-500/dataset/01_alb_id.zip'
+        save_path: 'data/file.zip'
+    """
+
+    # create save_dir if not present
+    create_dir(os.path.dirname(save_path))
+    # download file
+    gdown.download(url, save_path, quiet=False)
+
+
+"""
+url = "https://download.pytorch.org/models/retinanet_resnet50_fpn_coco-eeacb38b.pth"
+url = "https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
+save_path = "../models/retinanet_resnet50_fpn_coco.pth"
+download(url, save_path)
+"""
